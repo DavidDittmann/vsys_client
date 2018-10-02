bf64e0798df977f1fc588f92d187d739d1f6d4fc
DavidDittmann <d.dittmann@gmx.at>
2018-10-02 18:15:35 +0200

    update login,send

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include "TCP.h"
 #include <iostream>
 #include <limits>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/time.h>
+
 
 using namespace std;
 
 bool clientLogic(TCP_Client* Client);
-void readMail(TCP_Client* Client);
-void delMail(TCP_Client* Client);
-void sendMail(TCP_Client* Client);
-void listMail(TCP_Client* Client);
+void loginLDAP(TCP_Client* Client,string &logedInUser);
+void readMail(TCP_Client* Client,const string &user);
+void delMail(TCP_Client* Client,const string &user);
+void sendMail(TCP_Client* Client,const string &user);
+void listMail(TCP_Client* Client,const string &user);
+
+
+
 
 int main(int argc, char* argv[]) {
 
@@ -47,9 +55,10 @@ bool clientLogic(TCP_Client* Client)
 {
     //CLIENT - LOGIK
     int option;
+    string logedInUser="ERROR_USER";
     cout << endl << "-----------------------------------------------------------------------------------------------" << endl;
     cout << "What do u want to do?" << endl;
-    cout << "(0) QUIT | (1) SEND new Mail | (2) LIST all Mails | (3) READ Mail | (4) DELETE Mail" << endl;
+    cout << "(0) QUIT | (1) SEND new Mail | (2) LIST all Mails | (3) READ Mail | (4) DELETE Mail | (5) LOGIN" << endl;
     do {
         if(cin.fail())
         {
@@ -65,31 +74,95 @@ bool clientLogic(TCP_Client* Client)
             cout << "Closing Client..." << endl;
             return false;
         case 1: //SEND
-            sendMail(Client);
+            sendMail(Client,logedInUser);
             break;
         case 2: //LIST
-            listMail(Client);
+            listMail(Client,logedInUser);
             break;
         case 3: //READ
-            readMail(Client);
+            readMail(Client,logedInUser);
             break;
         case 4: //DELETE
-            delMail(Client);
+            delMail(Client,logedInUser);
+            break;
+        case 5: // LOGIN
+            loginLDAP(Client,logedInUser);
             break;
         default:
+            cout << "False Input!" << endl;
             break;
     }
 
     return true;
 }
 
-void readMail(TCP_Client* Client){
+void loginLDAP(TCP_Client* Client,string &logedInUser) {
+    string username="";
+    cout << "Enter FHTW username (max 8 letters):";
+    do{
+        cin >> username;
+    }while(username.length()>8);
+
+    cin.clear();cin.ignore();
+
+    struct termios oflags, nflags;
+    char password[64];
+
+    /* disabling echo */
+    tcgetattr(fileno(stdin), &oflags);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
+
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
+        perror("tcsetattr");
+        //return EXIT_FAILURE;
+    }
+
+    printf("Enter password: ");
+    fgets(password, sizeof(password), stdin);
+    password[strlen(password) - 1] = 0;
+
+    /* restore terminal */
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0) {
+        perror("tcsetattr");
+        //return EXIT_FAILURE;
+    }
+
+    string message = "";
+    message += "LOGIN\n";
+    message += username + "\n";
+    message += password; message += "\n";
+
+    Client->sendData(message);
+    char result[10]; int size = 10;
+    memset(result,0,sizeof(char)*size);
+    Client->recvData(size,result);
+
+    if(strcmp (result,"OK\n") == 0)
+    {
+        cout << "Server OK" << endl;
+        logedInUser = username;
+    }
+    else if(strcmp (result,"ERR\n") == 0)
+    {
+        cout << "Server ERR" << endl;
+        logedInUser="ERROR_USER";
+    }
+    else
+    {
+        cout << "Other error" << endl;
+        logedInUser="ERROR_USER";
+    }
+}
+
+void readMail(TCP_Client* Client,const string &user){
 
 }
-void delMail(TCP_Client* Client){
+void delMail(TCP_Client* Client,const string &user){
 
 }
-void sendMail(TCP_Client* Client){
+void sendMail(TCP_Client* Client,const string &user){
     string to= "a";
     string subject = "a";
     string msg = "a";
@@ -115,6 +188,7 @@ void sendMail(TCP_Client* Client){
 
     //LOGINNAME FEHLT!!!
     mail = "SEND\n";
+    mail += user + '\n';
     mail += to + '\n';
     mail += subject + '\n';
     mail += msg + "\n.\n";
@@ -124,19 +198,21 @@ void sendMail(TCP_Client* Client){
     memset(result,0,sizeof(char)*size);
     Client->recvData(size,result);
 
-    if(result == "OK\n")
+    if(strcmp (result,"OK\n") == 0)
     {
-
+        cout << "Server OK" << endl;
     }
-    else if(result == "ERR\n")
+    else if(strcmp (result,"ERR\n") == 0)
     {
-
+        cout << "Server ERROR" << endl;
     }
     else
     {
-
+        cout << "Other error" << endl;
     }
 }
-void listMail(TCP_Client* Client){
+void listMail(TCP_Client* Client,const string &user){
 
 }
+
+
