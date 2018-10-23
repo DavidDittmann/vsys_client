f02f51fd6415c18c4f5d9a3f1e71053f170e965b
DavidDittmann <d.dittmann@gmx.at>
2018-10-23 14:49:57 +0200

    major updates functunality missing: delete, multithreading ready

--- main.cpp
+++ main.cpp
@@ -11,19 +11,19 @@ OK das ist ein 2. Test
 
 using namespace std;
 
-bool clientLogic(TCP_Client* Client);
+bool clientLogic(TCP_Client* Client, string &logenInUser);
 void loginLDAP(TCP_Client* Client,string &logedInUser,int &failedLogins);
 void readMail(TCP_Client* Client,const string &user);
 void delMail(TCP_Client* Client,const string &user);
 void sendMail(TCP_Client* Client,const string &user);
 void listMail(TCP_Client* Client,const string &user);
-
+int cinNumber(void);
 
 
 
 int main(int argc, char* argv[]) {
 
-    if(argc != 2)
+    if(argc != 3)
         cout << "Usage: ./Client <PortNr.> <Address>" << endl;
 
     int port = stoi(argv[1]);
@@ -39,9 +39,10 @@ int main(int argc, char* argv[]) {
             cout << "Connected to host: " << addr << ":" << port << endl;
         }
         bool ret = true;
+        string user = "ERROR_USER";
         while(ret)
         {
-            ret = clientLogic(Client);
+            ret = clientLogic(Client,user);
         }
     }
     catch(runtime_error &e){
@@ -54,53 +55,56 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-bool clientLogic(TCP_Client* Client)
+bool clientLogic(TCP_Client* Client,string &logedInUser)
 {
     //CLIENT - LOGIK
     int failedLogins = 0;
-    int option;
-    string logedInUser="ERROR_USER";
+    int option=0;
+    bool ret = true;
     cout << endl << "-----------------------------------------------------------------------------------------------" << endl;
     cout << "What do u want to do?" << endl;
     cout << "(0) QUIT | (1) SEND new Mail | (2) LIST all Mails | (3) READ Mail | (4) DELETE Mail | (5) LOGIN" << endl;
-    do {
-        if(cin.fail())
-        {
-            cout << "U must enter a number!" <<endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }
-        cin >> option;
-    }while(cin.fail());
+    option = cinNumber();
     switch(option)
     {
-        case 0:
+        case 0: {
             cout << "Closing Client..." << endl;
             string msg = "QUIT\n";
             Client->sendData(msg);
-            delete(Client);
-            return false;
+            ret = false;
+            break;
+        }
         case 1: //SEND
-            sendMail(Client,logedInUser);
+        {
+            sendMail(Client, logedInUser);
             break;
+        }
         case 2: //LIST
-            listMail(Client,logedInUser);
+        {
+            listMail(Client, logedInUser);
             break;
+        }
         case 3: //READ
-            readMail(Client,logedInUser);
+        {
+            readMail(Client, logedInUser);
             break;
+        }
         case 4: //DELETE
-            delMail(Client,logedInUser);
+        {
+            delMail(Client, logedInUser);
             break;
+        }
         case 5: // LOGIN
-            loginLDAP(Client,logedInUser,failedLogins);
+        {
+            loginLDAP(Client, logedInUser, failedLogins);
             break;
+        }
         default:
             cout << "False Input!" << endl;
             break;
     }
 
-    return true;
+    return ret;
 }
 
 void loginLDAP(TCP_Client* Client,string &logedInUser, int &failedLogins) {
@@ -161,70 +165,180 @@ void loginLDAP(TCP_Client* Client,string &logedInUser, int &failedLogins) {
         }
         logedInUser="ERROR_USER";
     }
-    else
+    else if(result!=NULL)
     {
-        cout << "Other error" << endl;
+
+        cout << result << endl;
         logedInUser="ERROR_USER";
     }
 }
 
 void readMail(TCP_Client* Client,const string &user){
-
-}
-void delMail(TCP_Client* Client,const string &user){
-
-}
-void sendMail(TCP_Client* Client,const string &user){
-    string to= "a";
-    string subject = "a";
-    string msg = "a";
-    string mail="";
-
-    cout << "Empfänger angeben: (max. 8 Zeichen)" << endl;
-    do{
-        if(to.length()>8 || to.length()==0)
-            cout << "Empfänger muss mehr als 0, aber max. 8 Zeichen lang sein" << endl;
-        to = "";
-        cin >> to;
-    }while(to.length()>8||to.length()==0);
-
-    cout << "Betreff angeben: (max. 80 Zeichen)" << endl;
-    do{
-        if(subject.length()>80 || subject.length()==0)
-            cout << "Betreff muss mehr als 0, aber max. 80 Zeichen lang sein" << endl;
-        subject = "";
-        cin >> subject;
-    }while(subject.length()>80||subject.length()==0);
-    cout << "Nachricht eingeben:" << endl;
-    cin >> msg;
-
-    //LOGINNAME FEHLT!!!
-    mail = "SEND\n";
-    mail += user + '\n';
-    mail += to + '\n';
-    mail += subject + '\n';
-    mail += msg + "\n.\n";
-
-    Client->sendData(mail);
-    char result[10]; int size = 10;
-    memset(result,0,sizeof(char)*size);
-    Client->recvData(size,result);
-
-    if(strcmp (result,"OK\n") == 0)
-    {
-        cout << "Server OK" << endl;
-    }
-    else if(strcmp (result,"ERR\n") == 0)
-    {
-        cout << "Server ERROR" << endl;
+    if(user=="ERROR_USER"){
+        cout << "Sie sind nicht eingeloggt!" << endl;
     }
     else
     {
-        cout << "Other error" << endl;
+        int msgNumber = -1;
+        cout << "Welche Nachricht wollen Sie lesen?" << endl;
+        msgNumber = cinNumber();
+
+        string msg = "READ\n"+user+"\n"+to_string(msgNumber)+"\n";
+        Client->sendData(msg);
+        char result[4098]; int size = 4098;
+        memset(result,0,sizeof(char)*size);
+        Client->recvData(size,result);
+        string response(result);
+
+        if(response.find("ERR\n")!=string::npos){
+            cout << "Server konnte Nachricht nicht finden" << endl;
+        }
+        else if(response.find("OK\n")!=string::npos)
+        {
+            string msg = response.substr(response.find("OK\n")+3);
+            cout << "Nachricht:\n" << msg << endl;
+        }
+        else
+            cout << "Other Error" << endl;
+    }
+}
+void delMail(TCP_Client* Client,const string &user){
+    if(user=="ERROR_USER"){
+        cout << "Sie sind nicht eingeloggt!" << endl;
+    }
+    else
+    {
+        int msgNumber = -1;
+        cout << "Welche Nachricht wollen Sie löschen?" << endl;
+        msgNumber = cinNumber();
+
+        string msg = "DEL\n"+user+"\n"+to_string(msgNumber)+"\n";
+        Client->sendData(msg);
+        char result[4098]; int size = 4098;
+        memset(result,0,sizeof(char)*size);
+        Client->recvData(size,result);
+        string response(result);
+
+        if(response.find("ERR\n")>=0){
+            cout << "Server konnte Nachricht nicht finden" << endl;
+        }
+        else if(response.find("OK\n")>=0)
+        {
+            cout << "Nachricht wurde gelöscht" << endl;
+        }
+        else
+            cout << "Other Error" << endl;
+    }
+}
+void sendMail(TCP_Client* Client,const string &user){
+    if(user=="ERROR_USER"){
+        cout << "Sie sind nicht eingeloggt!" << endl;
+    }
+    else {
+        string to = "a";
+        string subject = "a";
+        string msg = "";
+        string mail = "", line="";
+
+
+        cout << "Empfänger angeben: (max. 8 Zeichen)" << endl;
+        do {
+            if (to.length() > 8 || to.length() == 0)
+                cout << "Empfänger muss mehr als 0, aber max. 8 Zeichen lang sein" << endl;
+            to = "";
+            cin >> to;
+        } while (to.length() > 8 || to.length() == 0);
+
+
+        cin.ignore();
+        cout << "Betreff angeben: (max. 80 Zeichen)" << endl;
+        do {
+            if (subject.length() > 80 || subject.length() == 0)
+                cout << "Betreff muss mehr als 0, aber max. 80 Zeichen lang sein" << endl;
+            subject = "";
+            getline(cin, subject);
+        } while (subject.length() > 80 || subject.length() == 0);
+
+        //cin.ignore();
+        cout << "Nachricht eingeben:" << endl;
+        bool tmp = false;
+        while (getline(cin, line)) {
+            if (line.length() == 0)
+                break;
+
+            msg +=line +"\n";
+        }
+
+        //LOGINNAME FEHLT!!!
+        mail = "SEND\n";
+        mail += user + '\n';
+        mail += to + '\n';
+        mail += subject + '\n';
+        mail += msg + ".\n";
+
+        cout << mail;
+
+        Client->sendData(mail);
+        char result[10];
+        int size = 10;
+        memset(result, 0, sizeof(char) * size);
+        Client->recvData(size, result);
+
+        if (strcmp(result, "OK\n") == 0) {
+            cout << "Server OK" << endl;
+        } else if (strcmp(result, "ERR\n") == 0) {
+            cout << "Server ERROR" << endl;
+        } else {
+            cout << "Other error" << endl;
+        }
     }
 }
 void listMail(TCP_Client* Client,const string &user){
+    if(user=="ERROR_USER"){
+        cout << "Sie sind nicht eingeloggt!" << endl;
+    }
+    else
+    {
+        string msg = "LIST\n"+user+"\n";
+        Client->sendData(msg);
 
+        char result[4098]; int size = 4098;
+        memset(result,0,sizeof(char)*size);
+        Client->recvData(size,result);
+        string response(result);
+
+        cout << "Nachrichten:" << endl;
+        int msgCount = 1, pos=0, i=0;
+        vector <string> subjects;
+        while((pos = response.find("\n")) != string::npos && i<= msgCount && msgCount != 0)
+        {
+            if(i == 0)
+            {
+                msgCount = stoi(response.substr(0,pos));
+            }
+            else
+            {
+                cout << i-1 << ": " << response.substr(0,pos) << endl;
+                subjects.push_back(response.substr(0,pos));
+            }
+            response.erase(0,pos+1);
+            i++;
+        }
+    }
 }
 
+int cinNumber()
+{
+    int option;
+    do {
+        if(cin.fail())
+        {
+            cout << "Es muss eine pos. Zahl eingegeben werden!" <<endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        cin >> option;
+    }while(cin.fail() || option < 0);
+    return option;
+}
 
